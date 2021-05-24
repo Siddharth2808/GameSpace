aafd2c631a68327b20c29d0785aa4e4ca5b53607
Siddharth2808 <c.siddharth@iitg.ac.in>
2021-05-25 01:27:04 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const Express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
