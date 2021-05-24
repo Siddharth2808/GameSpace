65900b2ad201223ddd175cadd134c79da56f5677
Siddharth2808 <c.siddharth@iitg.ac.in>
2021-05-25 02:06:05 +0530

    View Engine

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controller/home_controllers');
 
+// setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 router.get('/', homeController.home);
 
 
