2a077d916e54445cda561ba5e9f03a6fc6eb5c77
Siddharth2808 <c.siddharth@iitg.ac.in>
2021-05-25 14:54:49 +0530

    Authentication

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware =require('node-sass-middleware');
+
+
 
 // setting up the view engine
 app.set('view engine', 'ejs');
