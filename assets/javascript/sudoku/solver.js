c5b8ea13ccf160f3e369aedafbb3ab81ddaa7d20
Siddharth2808 <76950378+Siddharth2808@users.noreply.github.com>
2021-05-28 17:18:51 +0530

    sudoku solver algo part  completed

--- assets/javascript/sudoku/solver.js
+++ assets/javascript/sudoku/solver.js
@@ -2,6 +2,7 @@ let originalBoard;
 let cellNow=-1;
 let gameover=0;
 var output = [];
+let nowop;
 //cells variable will now contain a reference to every element with a class="cell"
 const cells = document.querySelectorAll('.cell');
 
@@ -44,11 +45,14 @@ function reply_click(clicked_id)
     if(clicked_id=="solve-button")
     {
         for(let i=0;i<81;i++)
-        {
-           output.push(cells[i].innerText);
+        {  
+            if(cells[i].innerText!="") output.push(parseInt(cells[i].innerText,10));
+            else output.push(-1);
         }
-        solve_final();
+        if(cellNow!=-1) document.getElementById(cellNow).style.background = 'cyan';
         gameover=1;
+        solve_final();
+        
     }
     if(gameover==0){
     var now = parseInt(clicked_id, 10);
@@ -59,9 +63,95 @@ function reply_click(clicked_id)
     //console.log(cellNow);
 }
 }
-
-
-solve_final()
-{
-    
+function isValidPlace(num)
+{   
+    let a=nowop%9; let b = nowop-a;let c,d;
+    c=(b/9)-((b/9)%3);d=(a)-((a)%3);
+    //console.log("a=",a,"b=",b);
+    for(let i=b;i<b+9;i++)
+    {
+        if(output[i]===num && i!=nowop) {return false;}
+    }
+    for(let i=a;i<=a+72;i+=9)
+    {
+        if(output[i]===num && i!=nowop) {return false;}
+    }
+    for(let k=c;k<c+3;k++)
+    {
+        for(let j=d;j<d+3;j++)
+        {
+             let i=k*9+j;
+             if(output[i]===num && i!=nowop) {return false;}
+        }
+    }
+    return true;
 }
+
+
+function findEmptyPlace()
+{
+    for( nowop=0;nowop<81;nowop++)
+    {
+        if(output[nowop]==-1)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function solve_sudoku()
+{
+    if (!findEmptyPlace())
+       return true;
+       let temp=nowop;
+       console.log(nowop);
+       for (let num = 1; num <= 9; num++){ 
+           nowop=temp;
+        if (isValidPlace(num)){ 
+           output[nowop] = num;
+           if (solve_sudoku()) {return true;}
+              output[nowop] = -1;
+        }
+     }
+    return false;
+}
+
+function checksyntex()
+{
+    for( let i=0;i<81;i++)
+    {nowop=i;
+        if(output[i]!=-1)
+        {
+            if(!isValidPlace(output[i])) {nowop=0;return false;}
+        }
+    }
+    nowop=0;
+    return true;
+}
+
+
+function solve_final()
+{
+    if(checksyntex()==true){
+    if (solve_sudoku() == true)
+    {
+        document.getElementById("result").innerText="Yay sucessfully able to find solution";
+         for(let i=100;i<=180;i++)
+         {
+             document.getElementById(i).innerText=output[i-100];
+         }
+    }
+    else
+    {
+        document.getElementById("result").innerText="No solution found";
+        
+    }
+}
+else
+{
+    document.getElementById("result").innerText="No solution found";
+}
+}
+
