df41560c3dd0ce044c1fecd23c7e2bd6e8fcf181
Siddharth2808 <76950378+Siddharth2808@users.noreply.github.com>
2021-06-06 17:17:26 +0530

    Added chat, profile pic feature and also added some more authentication

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const usersController = require('../controller/users_controller');
 
 router.get('/profile', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
@@ -18,5 +19,7 @@ router.post('/create-session', passport.authenticate(
     {failureRedirect: '/users/sign-in'},
 ), usersController.createSession);
 
+
 router.get('/sign-out', usersController.destroySession);
+
 module.exports = router;
