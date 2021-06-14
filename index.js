287231209373309d0ba210a5f744683f74a230d1
ShreyV25 <shrey.verma2509@gmail.com>
2021-06-14 19:27:36 +0530

    -

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // used for session cookie
