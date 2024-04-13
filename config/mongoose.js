415ec9879a88c629c6de14bedb9b86b1e02cc132
Vijay Jangal <jangalvijay36@gmail.com>
2024-04-14 05:03:35 +0530

    "chnaged the ui"

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,9 @@ mongoose.connect("mongodb://localhost:27017/gamespace", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 const db = mongoose.connection;
 
